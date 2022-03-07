6fbdb025c4a8f20fbb3f881290c40cc2f8bd2b50
Sergio D. Barrera <sdbeng@gmail.com>
2022-03-06 17:17:57 -0800

    Add mock and Player constructor

---
+++ __tests__/Player.tests.js
@@ -0,0 +1,19 @@
+const Player = require('../lib/Player')
+const Potion = require('../lib/Potion')
+jest.mock('../lib/Potion')
+
+//log a mocked potion
+console.log(new Potion());
+
+test('creates a player obj', () => { 
+    const player = new Player('Dave')
+
+    expect(player.name).toBe('Dave')
+    expect(player.health).toEqual(expect.any(Number))
+    expect(player.strength).toEqual(expect.any(Number))
+    expect(player.agility).toEqual(expect.any(Number))
+    // check for creation of an inventory
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    )
+ })
